302631bfa8088b550245f9a2db9e12e242741a19
joep-source <joepleonardo@live.nl>
2020-12-19 14:20:30 +0100

    leds dont turn on at night

--- leds.py
+++ leds.py
@@ -1,14 +1,22 @@
 import RPi.GPIO as GPIO
+import datetime as dt
 
 LED_R = 29
 LED_Y = 31
 LED_G = 33
 
+NIGHT_LEDS_OFF = True
+NIGHT_START_HOUR = 23
+NIGHT_END_HOUR = 7
+
+
 def setup():
     GPIO.setmode(GPIO.BOARD)
+    GPIO.setwarnings(False)
     GPIO.setup(LED_R, GPIO.OUT)
     GPIO.setup(LED_Y, GPIO.OUT)
     GPIO.setup(LED_G, GPIO.OUT)
+    
     
 def off(led=None):
     if not led:
@@ -19,11 +27,17 @@ def off(led=None):
         GPIO.output(led, GPIO.LOW)
     
 
-def on(led=None):
+def on(led=None):    
+    # skip turn leds on at night?
+    if NIGHT_LEDS_OFF:
+        hour = dt.datetime.now().hour
+        if hour>=NIGHT_START_HOUR or hour<NIGHT_END_HOUR:
+            return
+
+    # turn leds on
     if not led:
         GPIO.output(LED_R, GPIO.HIGH)
         GPIO.output(LED_Y, GPIO.HIGH)
         GPIO.output(LED_G, GPIO.HIGH)
     else:
         GPIO.output(led, GPIO.HIGH)
-        
